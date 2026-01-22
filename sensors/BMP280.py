c2d87e4ee4b7b03ba23f1b7c0bb8a347a9af0111
arnaualabau <arnau.aas@gmail.com>
2026-01-23 00:12:36 +0100

    fixes

--- sensors/BMP280.py
+++ sensors/BMP280.py
@@ -4,3 +4,7 @@
 # Author : Arnau Alabau Serra
 # Date   : 01/01/2026
 # --------------------------------------
+
+class BMP280:
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("BMP280 sensor not implemented yet")
